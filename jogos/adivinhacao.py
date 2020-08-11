7107d8bf4331f94da9fbb8e2aa034c00b2bf22d7
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-11 15:26:41 -0300

    import random

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,9 +1,12 @@
+import random
+
 print("\n##################################")
 print("Bem vindos ao jogo da Adivinhação!")
 print("##################################")
 
-numero_secreto = 42
+numero_secreto = round(random.random() * 100)
 total_de_tentativas = 3
+print("numero_secreto")
 
 for rodada in range (1, total_de_tentativas + 1):
 
@@ -15,7 +18,7 @@ for rodada in range (1, total_de_tentativas + 1):
     if (chute < 1 or chute > 100):
         print("\nVocê deve digitar um número entre 1 e 100\n")
         continue
-    
+
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
@@ -30,4 +33,5 @@ for rodada in range (1, total_de_tentativas + 1):
             print("\nVocê errou! O seu chute foi menor que o número secreto")
     print("\n--------------------------------------------------------------")
 
+print("O numero secreto era: {}".format(numero_secreto))
 print("\nO Jogo Terminou !!!")
