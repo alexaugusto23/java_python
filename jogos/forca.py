24af8c1a27f72a32ccec9326d3e60ff502ef0838
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-13 00:07:47 -0300

    inclusao de modulos e arquivo principal

---
+++ jogos/forca.py
@@ -0,0 +1,11 @@
+def jogar():
+
+    print("\n##################################")
+    print("Bem vindos ao jogo da Forca!")
+    print("\n##################################")
+
+
+    print("\nO Jogo Terminou !!!")
+
+if (__name__ == "__main__"):
+    jogar()
