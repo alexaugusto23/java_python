06879963bf45acd549f5f19e6eb43f121be1c74b
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-19 01:00:15 -0300

    realizando bloco de comparação

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,6 +4,19 @@ def jogar():
     print("Bem vindos ao jogo da Forca!")
     print("\n##################################")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+
+    while (not enforcou and not acertou):
+        chute = input("Qual a letra: ")
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a Letra |{}| na posição {}".format(letra, index))
+            index += 1
+        print("jogando...")
 
     print("\nO Jogo Terminou !!!")
 
