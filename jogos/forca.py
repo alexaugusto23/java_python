b7dbd1498d9d21004dfdce98c3d7c625b6d35ebe
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-19 14:50:13 -0300

    for

--- jogos/forca.py
+++ jogos/forca.py
@@ -11,11 +11,12 @@ def jogar():
 
     while (not enforcou and not acertou):
         chute = input("Qual a letra: ")
+        chute = chute.strip()
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
-                print("Encontrei a Letra |{}| na posição {}".format(letra, index))
-            index += 1
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a Letra |{}| na posição |{}|".format(letra, index))
+            index + 1
         print("jogando...")
 
     print("\nO Jogo Terminou !!!")
