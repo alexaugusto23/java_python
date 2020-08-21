367faeec11555649f02d74b5a5515d71a6d987dc
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-08-21 17:32:31 -0300

    nova funcionalidade |dica|

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,24 +4,37 @@ def jogar():
     print("Bem vindos ao jogo da Forca!")
     print("\n##################################")
 
-    lista = []
-    for item in palavra_secreta:
-        lista.append("_")
-    print(lista)
-    
+    palavra_secreta = "banana"
+    letras_acertadas = []
 
     enforcou = False
     acertou = False
 
+    for i in palavra_secreta:
+        letras_acertadas.append("_")
+    print("Você deseja dicas ? \n")
+    dica = int(input("Sim 1 | Não 2: "))
+    if (dica == 1):
+        print("\nDica - 1 = Quantidade de palavras = ", len(palavra_secreta))
+        for i in letras_acertadas:
+            print(i, end = " ")
+        print("\n")
+    else:
+        print("\nContinue Jogando...\n")
 
     while (not enforcou and not acertou):
         chute = input("Qual a letra: ")
         chute = chute.strip()
+
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a Letra |{}| na posição |{}|".format(letra, index))
-            index + 1
+                letras_acertadas[index] = letra
+
+            index += 1
+
+        print(letras_acertadas)
+        
         print("jogando...")
 
     print("\nO Jogo Terminou !!!")
