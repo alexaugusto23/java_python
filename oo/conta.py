8b72b89ff92f6f9e9947d5a71f3bda1fc92ad735
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-03 18:00:34 -0300

    Getters e Setters da classe conta e classe retangulo.

--- oo/conta.py
+++ oo/conta.py
@@ -1,37 +1,44 @@
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
-        #print("Construindo objeto...{}".format(self))
         # Atributos são caracteristicas que o objeto tem.
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        print("Construindo objeto...{}".format(self)) 
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
         # Incluindo métodos que são comportamnetos que o objeto possui a partir de seus atributos.
         # self = referência (.ponto) --> acessa atributo (váriavel) 
     def extrato(self):
         #print("Saldo {} do titular {}".format(self.saldo, self.titular))
-        print(f"O Saldo do titular {self.titular} é: {self.saldo}")
+        print(f"O Saldo do titular {self.__titular} é: {self.__saldo}")
 
     def deposita(self, valor):
-        self.saldo += valor
-    
+        self.__saldo += valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+        pass
+
     def saca(self, valor):
-        if (self.saldo <= 0):
-            if (valor > self.limite):
-                print("Sem saldo")
-                print(f"Seu saldo é: {self.saldo} ")
-                self.saldo -= valor
-                print(f"Limite disponivel é: {self.limite} ")
-                self.saldo -= - self.limite
+        self.__saldo -= valor
+    
+    #Getters e Setters
+    def devolve_saldo(self):
+        return self.__saldo
+    
+    def devolve_titular(self):
+        pass
+
+    def devolve_titular(self):
+        pass
 
 
-        else:
-            self.saldo -= valor 
 
 #conta1 é uma referência na memória para a classe(objeto) Conta
-conta1 = Conta(1,"Alex", 0.00, 400.00)
+conta1 = Conta(1,"Alex", 30.00, 400.00)
 conta2 = Conta(2,"Ana", 10.00, 600.00)
 conta3 = Conta(3, "Sicrano", 2000.0, 2000.0)
 
@@ -45,5 +52,13 @@ conta3 = Conta(3, "Sicrano", 2000.0, 2000.0)
 #conta1.extrato()
 #conta1.deposita(100.00)
 #conta1.extrato()
-conta1.saca(390.00)
-conta1.extrato()
+
+#conta2.extrato()
+#conta3.extrato()
+#conta3.transfere(100.00,conta2)
+#conta2.extrato()
+#conta3.extrato()
+
+
+
+
