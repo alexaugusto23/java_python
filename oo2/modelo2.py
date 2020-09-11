c37c7d42857cc0961976fb2530a43d4e4e305d7d
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-11 16:46:14 -0300

    herança modelo 2

--- oo2/modelo2.py
+++ oo2/modelo2.py
@@ -1,8 +1,7 @@
-class Filme:
-    def __init__(self, nome, ano, duracao):
+class Programa:
+    def __init__(self, nome, ano):
         self.__nome = nome.title()
         self.ano = ano 
-        self.duracao = duracao
         self.__likes = 0
 
     @property
@@ -21,12 +20,13 @@ class Filme:
         self.__likes +=1
 
 
-vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-print(f'\nNome: {vingadores.nome}'
-      f'\nAno: {vingadores.ano}'
-      f'\nDuração: {vingadores.duracao}'
-      f'\nLikes: {vingadores.likes}')
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano 
+        self.duracao = duracao
+        self.__likes = 0
+
 
 class Serie:
     def __init__(self, nome, ano, temporadas):
@@ -35,20 +35,13 @@ class Serie:
         self.temporadas = temporadas
         self.__likes = 0
 
-    @property
-    def nome(self):
-        return self.__nome
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
-
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_like(self):
-        self.__likes +=1
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(f'\nNome: {vingadores.nome}'
+      f'\nAno: {vingadores.ano}'
+      f'\nDuração: {vingadores.duracao}'
+      f'\nLikes: {vingadores.likes}')
 
 atantla = Serie('atantla', 2017, 2)
 atantla.dar_like()
