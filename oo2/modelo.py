74d0384e5c9ed1b4a73ea5de639b01c1efb210fd
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-09-11 15:55:47 -0300

    inserção de @property

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -1,14 +1,37 @@
 class Filme:
     def __init__(self, nome, ano, duracao):
-        self..nome = nome
+        self.__nome = nome.title()
         self.ano = ano 
         self.duracao = duracao
+        self.__likes = 0
+    
+    def
+    
+    def dar_like(self):
+        self.__likes +=1
 
-vingadores = Filme()
-print(vingadores)
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+print(f'\nNome: {vingadores.nome}'
+      f'\nAno: {vingadores.ano}'
+      f'\nDuração: {vingadores.duracao}'
+      f'\nLikes: {vingadores.likes}')
 
 class Serie:
-    def __init__(self, nome, ano, temporada):
-        self..nome = nome
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
         self.ano = ano 
-        self.temporada = temporada
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    def dar_like(self):
+        self.__likes +=1
+
+atantla = Serie('atantla', 2017, 2)
+atantla.dar_like()
+atantla.dar_like()
+print(f'\nNome: {atantla.nome}'
+      f'\nAno: {atantla.ano}'
+      f'\nTemporadas: {atantla.temporadas}'
+      f'\nLikes: {atantla.likes}')
