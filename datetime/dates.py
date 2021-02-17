e62bd0d14ae5df52e6fa8ce08d65cf00121526f9
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-17 17:32:06 -0300

    formantando datas

--- datetime/dates.py
+++ datetime/dates.py
@@ -1,12 +1,19 @@
 from datetime import date
+from typing import MutableSequence
 
 def data():
     data_atual = date.today()
+    dia = data_atual.day 
+    mes = data_atual.month
+    ano = data_atual.year
+    format = None
+    print(dia < 10)
+    if dia  < 10: d = 0
+    else: d = ''
+    if mes  < 10: m = 0
+    else: m = ''
     print(data_atual)
-    print
+    print('{}{}/{}{}/{}'.format(d,dia,m,mes,ano))
 
 if __name__ == "__main__":
     data()
-
-
-
