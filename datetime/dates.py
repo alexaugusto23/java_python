00d29cc8a64e3310b8f7353438450384a22f0d94
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-17 16:49:21 -0300

    trabalhando com datas

---
+++ datetime/dates.py
@@ -0,0 +1,12 @@
+from datetime import date
+
+def data():
+    data_atual = date.today()
+    print(data_atual)
+    print
+
+if __name__ == "__main__":
+    data()
+
+
+
--- oo2/modelo_classe_abstrata.py
+++ oo2/modelo_classe_abstrata.py
@@ -2,11 +2,21 @@ from abc import ABC # abstract base classes
 from collections.abc import MutableSequence
 from numbers import(Complex)
 
+
 class Playlist(MutableSequence):
     pass
+
 
 class Numero(Complex):
     def __getitem__ (self, item):
         super().__getitem__()
 
+
+from abc import ABCMeta, abstractmethod 
+class Programa(metaclass = ABCMeta): 
+    @abstractmethod 
+    def __str__(self): 
+        pass
+
 filmes = Playlist()
+num = Numero()
