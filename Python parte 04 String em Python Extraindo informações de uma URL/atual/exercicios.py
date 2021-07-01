8a96f83febf67b73cb2e3839b52ae44cadc1d460
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-01 18:25:10 -0300

    exercicios

---
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/exercicios.py
@@ -0,0 +1,4 @@
+nome_completo = "Gabriel Saldanha"
+primeiro_nome = nome_completo[0:7]
+ultimo_nome = nome_completo[8:15]
+print(f"{}{}")
--- Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
@@ -1,5 +1,8 @@
 url = "bytebank.com/cambio?moedaOrigem=real"
 url_base = url[0:19]
 url_parametros = url[20:]
+url_parametros = url[20:36]
 
 print(f"\nURL: {url}\n\nURL_base: {url_base}\n\nURL_parametros: {url_parametros}\n")
+print(f"{url}")
+
