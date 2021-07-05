ec34cbe63a56347540949718fdfd3690d67d3080
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-05 16:27:28 -0300

    metodo len

--- Python parte 04 String em Python Extraindo informações de uma URL/atual/exercicios.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/exercicios.py
@@ -1,4 +1,14 @@
+url = "bytebank.com/cambio?moedaOrigem=real"
+url_base = url[0:19]
+url_parametros = url[20:]
+url_parametros = url[20:36]
+
+print(f"\nURL: {url}\n\nURL_base: {url_base}\n\nURL_parametros: {url_parametros}\n")
+print(f"{url}")
+
+
 nome_completo = "Gabriel Saldanha"
 primeiro_nome = nome_completo[0:7]
-ultimo_nome = nome_completo[8:15]
-print(f"{}{}")
+ultimo_nome = nome_completo[8:16]
+#print(f"{primeiro_nome} {ultimo_nome}")
+
--- Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
@@ -1,8 +1,9 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-url_base = url[0:19]
+url = "https://bytebank.com/cambio?moedaOrigem=real"
+ind_interrogacao = url.find('?')
+url_base = url[0:ind_interrogacao]
 url_parametros = url[20:]
-url_parametros = url[20:36]
+url_parametros = url[ind_interrogacao+1:]
+url_parametros = url[ind_interrogacao+1:len(url)]
 
 print(f"\nURL: {url}\n\nURL_base: {url_base}\n\nURL_parametros: {url_parametros}\n")
 print(f"{url}")
-
