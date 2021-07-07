ea1b11cf9b3dc4b237f042aab8e062438a320280
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-07 14:03:59 -0300

    2 modulo

--- Python parte 04 String em Python Extraindo informações de uma URL/atual/exercicios.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/exercicios.py
@@ -1,14 +1,26 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-url_base = url[0:19]
+url = "https://bytebank.com/cambio?moedaOrigem=real"
+ind_interrogacao = url.find('?')
+url_base = url[0:ind_interrogacao]
 url_parametros = url[20:]
-url_parametros = url[20:36]
+url_parametros = url[ind_interrogacao+1:]
+url_parametros = url[ind_interrogacao+1:len(url)]
 
 print(f"\nURL: {url}\n\nURL_base: {url_base}\n\nURL_parametros: {url_parametros}\n")
 print(f"{url}")
 
+###########################################################
 
 nome_completo = "Gabriel Saldanha"
 primeiro_nome = nome_completo[0:7]
 ultimo_nome = nome_completo[8:16]
 #print(f"{primeiro_nome} {ultimo_nome}")
 
+###########################################################
+url = ''
+if len(url) > 0:
+    print("A URL contél algum valor")
+else:
+    print("A URL está vazia!")
+
+if len(url) == 0:
+    print("A URL está vazia!")
--- Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
@@ -1,9 +1,23 @@
-url = "https://bytebank.com/cambio?moedaOrigem=real"
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
 ind_interrogacao = url.find('?')
 url_base = url[0:ind_interrogacao]
-url_parametros = url[20:]
 url_parametros = url[ind_interrogacao+1:]
-url_parametros = url[ind_interrogacao+1:len(url)]
+#print(f"{url_parametros}")
+# url_parametros = url[ind_interrogacao+1:len(url)]
 
-print(f"\nURL: {url}\n\nURL_base: {url_base}\n\nURL_parametros: {url_parametros}\n")
-print(f"{url}")
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+moedaOrigem = url_parametros[indice_valor:]
+print(f"{url_parametros}")
+print(f"{indice_parametro}")
+print(f"len: {indice_valor}")
+print(moedaOrigem)
+
+parametro_busca = 'moedaDestino'
+parametro_busca_dois = '&'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_parametro_final = url_parametros.find(parametro_busca_dois)
+indice_valor = len(parametro_busca) + 1
+moedaDestino = url_parametros[indice_valor:indice_parametro_final]
+print(moedaDestino)
