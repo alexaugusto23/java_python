8d421bea249839386d5fc71e8b051bc5c9647371
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-14 12:13:55 -0300

    aula 04 finalizada.

--- Python parte 04 String em Python Extraindo informações de uma URL/atual/extrator_url.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/extrator_url.py
@@ -5,22 +5,40 @@ class EstratorURL:
         self.valida_url()
 
     def sanitiza_url(self, url):
-        return url.strip()
+        if type(url) == str:
+            return url.strip()
+        else: return ""
 
     def valida_url(self):
-        if self.url == None:
-            raise ValueError("A URL é None") 
-        elif self.url == "":
+        if not self.url:
             raise ValueError("A URL está vazia") 
 
     def get_url_base(self):
-        pass
+        ind_interrogacao = self.url.find('?')
+        url_base = self.url[0:ind_interrogacao]
+        return url_base
 
     def get_url_parametros(self):
-        pass
+        ind_interrogacao = self.url.find('?')
+        url_parametros = self.url[ind_interrogacao + 1: ]
+        return url_parametros
+    
+    def get_valor_parametro(self, parametro_de_busca):
+        indice_de_parametro = self.get_url_parametros().find(parametro_de_busca)
+        indice_de_valor = indice_de_parametro + len(parametro_de_busca) + 1
+        indice_e_comercial = self.get_url_parametros().find('&', indice_de_valor)
+        valor = self.get_url_parametros()[indice_de_valor:indice_e_comercial]
+
+        if indice_e_comercial == -1:
+            valor = self.get_url_parametros()[indice_de_valor:]
+        else:
+            valor = self.get_url_parametros()[indice_de_valor:indice_e_comercial]
+        return valor
 
 
 
+url = "https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real"
+extrator_url = EstratorURL(None)
+moeda = extrator_url.get_valor_parametro("moedaOrigem")
 
-extrator_url = EstratorURL(" asd  ")
-print(extrator_url)
+print(f"URL: {extrator_url.url}\nMOEDA: {moeda}")
