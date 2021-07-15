374c9bddb8a49cf860ac784a03fb419d27bdff53
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-15 15:40:02 -0300

    finalizando módulo 05 de coleções.

--- Python parte 04 String em Python Extraindo informações de uma URL/atual/conversor_de_moeda.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/conversor_de_moeda.py
@@ -1,7 +1,7 @@
-def conv_real_dolar(valor, txdolar):
-    resultado = float(valor) * float(txdolar)
+def conv_real_dolar(valor, txreal):
+    resultado = float(valor) * float(txreal)
     return resultado
 
-def conv_dolar_real(valor, txreal):
-    resultado = float(valor) * float(txreal)
+def conv_dolar_real(valor, txdolar):
+    resultado = float(valor) * float(txdolar)
     return resultado 
--- Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
+++ Python parte 04 String em Python Extraindo informações de uma URL/atual/main.py
@@ -2,21 +2,20 @@
 from extrator_de_url import ExtratorURL 
 from conversor_de_moeda import conv_dolar_real, conv_real_dolar
 
-url = "https://bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+url = "https://bytebank.com/cambio?quantidade=100&moedaOrigem=dolar&moedaDestino=real"
 extrator_url = ExtratorURL(url)
 
-tx_real = 0.1968503937007874
-tx_dolar = 5.08  
+tx_real = 0.19696469526016277
+tx_dolar = 5.0771
 moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
 moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
 quantidade = extrator_url.get_valor_parametro("quantidade")
 
 if moeda_origem.lower() == 'real' and moeda_destino.lower() == 'dolar':
-    valor =  conv_real_dolar(quantidade, tx_dolar)
-    print(f"{round(valor, 2)}")
+    valor =  conv_real_dolar(quantidade, tx_real)
+    print(f"{valor:.2f}")
 elif moeda_origem.lower() == 'dolar' and moeda_destino.lower() == 'real':
-    valor =  conv_dolar_real(quantidade, tx_real)
-    print(f"{round(valor, 2)}")
+    valor =  conv_dolar_real(quantidade, tx_dolar)
+    print(f"{valor:.2f}")
 else:
     print(f"Câmbio de {moeda_origem} para {moeda_destino} não está disponível.")
-
